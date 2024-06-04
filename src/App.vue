4872ea3371037df1f02437d34266e30de2f679a8
Stanislav <evgenija.khamrik@gmail.com>
2024-06-04 21:32:17 +0300

    feat: homework-13

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,18 @@
 <template>
-    <ListGroup>
-        <template v-slot:navbar>
-            <nav class="navbar navbar-expand-lg bg-success-subtle">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                   aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-        </template>
-
-        <template v-slot:list-childrens>
-            <li class="list-group-item" v-for="item of listData" :key="item.id" @click="getActiveItem($event)">{{item.text}}</li>
-        </template>
-    </ListGroup>
+    <h1>Button group:</h1>
+    <BtnGroup/>
 </template>
 
 <script>
-    import ListGroup from './components/ListGroup.vue'
-    import $ from 'jquery';
+    import BtnGroup from './components/BtnGroup.vue'
 
     export default {
         name: 'App',
         components: {
-            ListGroup
+            BtnGroup
         },
         data() {
-            return {
-                listData: [
-                    {id: 1, text: 'An item'},
-                    {id: 2, text: 'A second item'},
-                    {id: 3, text: 'A third item'},
-                    {id: 4, text: 'A fourth item'},
-                    {id: 5, text: 'And a fifth one'}
-                ]
-            }
-        },
-        methods:{
-            getActiveItem(event){
-                let items = $('.list-group-item');
-                items.removeClass('active');
-                event.target.classList.add('active');
-
-            }
+            return {}
         }
     }
 </script>
