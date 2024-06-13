dae1e00a7778ae9eb3d71ebe9dec35482d4f738b
Stanislav <evgenija.khamrik@gmail.com>
2024-06-14 00:03:11 +0300

    feat: homework-16

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <CaruselComponent :links="imgLinks"/>
+    <img alt="Vue logo" style="width: 70px" src="./assets/logo.png">
+    <h1 class="mt-2">Collapse component:</h1>
+    <collapse-element :text="text" :opened="opened"/>
 </template>
 
 <script>
-    import CaruselComponent from './components/CaruselComponent.vue'
+    import CollapseElement from "@/components/CollapseElement";
 
     export default {
         name: 'App',
         components: {
-            CaruselComponent
+            CollapseElement,
         },
         data() {
             return {
-                imgLinks: [
-                    {id: 1, link: 'one.jpg'},
-                    {id: 2, link: 'two.jpg'},
-                    {id: 3, link: 'three.jpg'},
-                    {id: 4, link: 'four.jpg'},
-                    {id: 5, link: 'five.jpg'},
-                ]
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quia, vero. Accusantium adipisci aperiam assumenda autem debitis, dolore eius error est expedita, inventore ipsam minus mollitia neque porro quas quo sed sequi velit, veniam voluptatum. Ad aperiam consequatur consequuntur esse, et nisi optio quaerat repellendus sit soluta sunt velit vero?',
+                opened: true,
             }
         }
     }
@@ -33,9 +29,5 @@
         text-align: center;
         color: #2c3e50;
         margin-top: 60px;
-    }
-
-    img {
-        max-width: 60px;
     }
 </style>
